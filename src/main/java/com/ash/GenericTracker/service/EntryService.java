87f7d9d1ea97e1555398f7495aabf502d925bd74
Ash <ash@a.com>
2026-03-11 23:52:38 +0530

    Checked the UI, need to write api based on the ui requirement!

--- src/main/java/com/ash/GenericTracker/service/EntryService.java
+++ src/main/java/com/ash/GenericTracker/service/EntryService.java
@@ -7,18 +7,24 @@ import com.ash.GenericTracker.entity.Parameter;
 import com.ash.GenericTracker.exception.CustomExceptionHandler;
 import com.ash.GenericTracker.repository.EntryRepository;
 import com.ash.GenericTracker.repository.EntryValueRepository;
+import com.ash.GenericTracker.repository.ParameterRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EntryService {
     private EntryRepository entryRepository;
     private EntryValueRepository entryValueRepository;
+    private ParameterRepository parameterRepository;
     public ApiResponse createEntry(Entry entry) {
-        if(!isValidDate()){
-            throw new CustomExceptionHandler("Enter Valid Date");
-        }
+//        if(!isValidDate()){
+//            throw new CustomExceptionHandler("Enter Valid Date");
+//        }
         try {
             entryRepository.save(entry);
         } catch (Exception e) {
@@ -33,8 +39,8 @@ public class EntryService {
 
     public ApiResponse createEntryValue(EntryValue entryValue) {
         try {
-           entryExist(entryValue.getEntry());
-           paramExist(entryValue.getParameter());
+//           entryExist(entryValue.getEntry());
+//           paramExist(entryValue.getParameter());
            if(isValidType(entryValue)){
                entryValueRepository.save(entryValue);
            }
@@ -44,13 +50,33 @@ public class EntryService {
         return new ApiResponse(true,"Inserted EntryValue Successfully",1);
     }
 
-    private void entryExist(Entry entry) {
-    }
-
-    private void paramExist(Parameter parameter) {
-    }
+//    private void entryExist(Entry entry) {
+//        Optional<Entry>res = entryRepository.findById(entry.getId());
+//        if(res.isEmpty()){
+//            log.error("Entry does not exist for entryValue : %s",entry.getId());
+//            throw new CustomExceptionHandler("Invalid Entry");
+//        }
+//    }
+//
+//    private void paramExist(Parameter parameter) {
+//        Optional<Parameter>res = parameterRepository.findById(parameter.getId());
+//        if(res.isEmpty()){
+//            log.error("Parameter does not exist for entryValue : %s",parameter.getId());
+//            throw new CustomExceptionHandler("Invalid Entry");
+//        }
+//    }
 
     private boolean isValidType(EntryValue entryValue) {
+        String dataType = entryValue.getParameter().getDataType();
+        Object valueType ;
+        if(entryValue.getValueText()!=null) valueType = entryValue.getValueText();
+        else if (entryValue.getValueTime()!=null) valueType = entryValue.getValueTime();
+        else valueType = entryValue.getValueNumber();
+
+        if (dataType.equalsIgnoreCase("TEXT") && (valueType instanceof String)  ||
+        dataType.equalsIgnoreCase("number") && (valueType instanceof Double)){
+            return true;
+        }
         return false;
     }
 }
